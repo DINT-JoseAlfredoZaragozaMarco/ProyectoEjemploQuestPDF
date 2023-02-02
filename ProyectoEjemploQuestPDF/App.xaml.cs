96b00e26a8e3c819eddcf4be5fec0b59a4ec36ce
JOSÉ ALFREDO ZARAGOZA MARCO <joszarmar@alu.edu.gva.es>
2023-02-02 12:51:52 +0100

    Agregar archivos de proyecto.

---
+++ ProyectoEjemploQuestPDF/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ProyectoEjemploQuestPDF
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            File.Delete("./Basura/imagen.jpg");
+        }
+    }
+}
---
+++ ProyectoEjemploQuestPDF/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProyectoEjemploQuestPDF
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerarPDF(object sender, RoutedEventArgs e)
+        {
+            //String ruta = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\imagen.jpg";
+            String ruta = "./Basura";
+
+            if (!File.Exists(ruta))
+            {
+                Directory.CreateDirectory(ruta);
+            }
+
+            ruta += "/imagen.jpg";
+            Document
+                .Create(document =>
+                {
+                    document.Page(page =>
+                    {
+                        page.Margin(1, Unit.Inch);
+
+                        page.Header()
+                            .AlignCenter()
+                            .Text(Titulo.Text)
+                            .FontSize(48)
+                            .SemiBold();
+
+                        page.Content()
+                            .Column( column =>
+                            {
+                                column.Spacing(0.5f, Unit.Inch);
+
+                                using (WebClient client = new WebClient())
+                                {
+                                    client.DownloadFile(new Uri("https://fototrending.com/wp-content/uploads/fotografia-minimalista.jpg"), ruta);
+                                }
+
+                                column.Item()
+                                    .Image(ruta);
+
+                                column.Item()
+                                    .Text(Placeholders.LoremIpsum())
+                                    .FontSize(18);
+                            });
+                        page.Footer()
+                            .AlignCenter()
+                            .Text(text =>
+                           {
+                               text.DefaultTextStyle(x => x.FontSize(18));
+
+                               text.CurrentPageNumber();
+                               text.Span(" / ");
+                               text.TotalPages();
+                           });
+                    });
+                }).GeneratePdf("Documento.pdf");
+        }
+    }
+}
